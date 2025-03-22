4ff5d66e49d8d754b4dba10243b7dac190a8b59e
Badhon deb <badhondeb100@gmail.coml>
2025-03-23 04:02:52 +0600

    task-2 done

---
+++ task-2.js
@@ -0,0 +1,25 @@
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+let places=["Peak","Bima shakti","Pochinok"]
+console.log(places);
+
+places.push("Clock tower")
+console.log(places);
+
+places.push("Observatory","Hanger")
+console.log(places);
+
+places.pop(places)
+console.log(places);
+
+
+
+
+// places.unshift("Observatory")
+// console.log(places);
+
+
